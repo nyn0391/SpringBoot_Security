d7adf8e4eef3982ee4c66e240dc4d52d83a9909c
nyn0391 <nayan.s.kadam@gmail.com>
2019-12-27 00:20:36 +0530

    first commit for gradle security

---
+++ spring-security-gradle/src/main/java/com/surb/nyn/security/springsecuritygradle/SpringSecurityGradleApplication.java
@@ -0,0 +1,13 @@
+package com.surb.nyn.security.springsecuritygradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGradleApplication.class, args);
+	}
+
+}
